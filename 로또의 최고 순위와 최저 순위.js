8d873ca0e34c5584f48232ecf7ab505e2f100986
KukjinKim0211 <rnrwls0211@naver.com>
2022-04-21 16:51:34 +0900

    sol

--- 로또의 최고 순위와 최저 순위.js
+++ 로또의 최고 순위와 최저 순위.js
@@ -1,14 +1,15 @@
 function solution(lottos, win_nums) {
   var answer = [];
-  let zeroCount = 0;
-  let winCount = 0;
+  let zeroCount = 0;    // 0이 몇개인지 저장
+  let winCount = 0;     // 숫자를 맞춘 개수가 몇개인지 저장
+
   for (let i = 0; i < 6; i++) {
-    if (lottos[i] === 0) {
+    if (lottos[i] === 0) {    // 번호가 0인 경우
       zeroCount++;
       continue;
     }
 
-    for (let j = 0; j < 6; j++) {
+    for (let j = 0; j < 6; j++) {     // 선택한 번호와 당첨 번호를 맞는지 체크
       if (lottos[i] === win_nums[j])
         winCount++;
     }
