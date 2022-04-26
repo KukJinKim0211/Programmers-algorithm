d39771c7af9b4b2988b55a0cdca251470b7ab6cc
KukjinKim0211 <rnrwls0211@naver.com>
2022-04-26 23:31:42 +0900

    sol

--- backjoon/BJ_3273.js
+++ backjoon/BJ_3273.js
@@ -1,11 +1,5 @@
 // 두 수의 합
-const readLine = require('readliine');
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-const data = require('fs').readFileSync('example.txt').toString().split(/\n/);
+const data = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\n/);
 const n = Number(data.shift());
 const x = Number(data.pop());
 const array = data[0].split(/\s/).map(v => +v).sort((a, b) => a - b);
